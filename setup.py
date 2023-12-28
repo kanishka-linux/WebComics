b3319ddf9c8a8fa316bf9625da8a2f1755237e73
kanishka-linux <kanishka.linux@gmail.com>
2023-12-28 22:27:53 +0530

    update dependencies and version

--- setup.py
+++ setup.py
@@ -18,8 +18,6 @@ along with kawaii-player.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 
-import os
-import shutil
 import platform
 from setuptools import setup
 
@@ -31,20 +29,25 @@ from setuptools import setup
 if platform.system().lower() == 'linux':
     install_dependencies = []
 else:
-    install_dependencies = ['PyQt5', 'bs4', 'Pillow', 'vinanti']
+    install_dependencies = [
+                'PyQt5',
+                'bs4',
+                'Pillow',
+                'vinanti @ git+ssh://git@github.com/kanishka-linux/vinanti.git'
+            ]
 setup(
-    name='WebComics', 
-    version='3.0.0', 
-    license='GPLv3', 
-    author='kanishka-linux', 
-    author_email='kanishka.linux@gmail.com', 
-    url='https://github.com/kanishka-linux/WebComics', 
+    name='WebComics',
+    version='4.0.0',
+    license='GPLv3',
+    author='kanishka-linux',
+    author_email='kanishka.linux@gmail.com',
+    url='https://github.com/kanishka-linux/WebComics',
     long_description="README.md",
-    packages=['WebComics'], 
+    packages=['WebComics'],
     entry_points={
-        'gui_scripts':['webcomics = WebComics.WebComics:main'], 
-        'console_scripts':['webcomics-console = WebComics.WebComics:main']
-        }, 
-    install_requires=install_dependencies, 
-    description="Desktop client for reading online comic strips", 
+        'gui_scripts': ['webcomics = WebComics.WebComics:main'],
+        'console_scripts': ['webcomics-console = WebComics.WebComics:main']
+        },
+    install_requires=install_dependencies,
+    description="Desktop client for reading online comic strips",
 )
